bec0a9455c256f716804fe9831670446d185cb23
MonsterGene <monster.j.zhang@mail.foxconn.com>
2020-09-10 11:02:08 +0800

    学完第14章

---
+++ src/views/book/components/Detail.vue
@@ -0,0 +1,190 @@
+<template>
+  <el-form ref="postForm" :model="postForm" label-suffix=":">
+    <sticky class-name="sub-navbar">
+      <el-button @click="showGuide">显示帮助</el-button>
+      <el-button
+        v-loading="loading"
+        type="success"
+        style="margin-left: 10px"
+        @click="submitForm"
+      >
+        {{ isEdit ? '编辑电子书' : '新增电子书' }}
+      </el-button>
+    </sticky>
+    <div class="detail-container">
+      <el-row>
+        <Warning />
+        <el-col :span="24">
+          <EbookUpload
+            :file-list="fileList"
+            :disabled="isEdit"
+            @onSuccess="onUploadSuccess"
+            @onRemove="onUploadRemove"
+          />
+        </el-col>
+        <el-col :span="24">
+          <el-form-item prop="title">
+            <md-input v-model="postForm.title" :max-length="100" required>
+              书名
+            </md-input>
+          </el-form-item>
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="作者" :label-width="labelWidth">
+                <el-input v-model="postForm.author" placeholder="作者" />
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item label="出版社" :label-width="labelWidth">
+                <el-input v-model="postForm.publisher" placeholder="出版社" />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="语言" :label-width="labelWidth">
+                <el-input v-model="postForm.language" placeholder="语言" />
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item label="根文件" :label-width="labelWidth">
+                <el-input v-model="postForm.rootFile" placeholder="根文件" disabled />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="文件路径" :label-width="labelWidth">
+                <el-input v-model="postForm.filePath" placeholder="文件路径" disabled />
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item label="解压路径" :label-width="labelWidth">
+                <el-input v-model="postForm.unzipPath" placeholder="解压路径" disabled />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="封面路径" :label-width="labelWidth">
+                <el-input v-model="postForm.coverPath" placeholder="封面路径" disabled />
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item label="文件名称" :label-width="labelWidth">
+                <el-input v-model="postForm.originalName" placeholder="文件名称" disabled />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="24">
+              <el-form-item label="封面" :label-width="labelWidth">
+                <a v-if="postForm.cover" :href="postForm.cover" target="_blank">
+                  <img :src="postForm.cover" class="preview-img">
+                </a>
+                <span v-else>无</span>
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="24">
+              <el-form-item label="目录" :label-width="labelWidth">
+                <div v-if="postForm.contents && postForm.contents.length > 0" class="contents-wrapper">
+                  <el-tree />
+                </div>
+                <span v-else>无</span>
+              </el-form-item>
+            </el-col>
+          </el-row>
+        </el-col>
+      </el-row>
+    </div>
+  </el-form>
+</template>
+
+<script>
+import Sticky from '../../../components/Sticky/index'
+import Warning from './Warning'
+import EbookUpload from '../../../components/EbookUpload/index'
+import MdInput from '../../../components/MDinput/index'
+
+export default {
+  components: {
+    Sticky,
+    Warning,
+    EbookUpload,
+    MdInput
+  },
+  props: {
+    isEdit: Boolean
+  },
+  data() {
+    return {
+      loading: false,
+      postForm: {},
+      fileList: [],
+      labelWidth: '120px'
+    }
+  },
+  methods: {
+    setData(data) {
+      const {
+        title,
+        author,
+        publisher,
+        language,
+        rootFile,
+        cover,
+        url,
+        originalName,
+        contents,
+        fileName,
+        coverPath,
+        filePath,
+        unzipPath
+      } = data
+      this.postForm = {
+        ...this.postForm,
+        title,
+        author,
+        publisher,
+        language,
+        rootFile,
+        cover,
+        url,
+        originalName,
+        contents,
+        fileName,
+        coverPath,
+        filePath,
+        unzipPath
+      }
+    },
+    onUploadSuccess(data) {
+      console.log('ebook upload success', data)
+      this.setData(data)
+    },
+    onUploadRemove() {
+      console.log('ebook remove success')
+    },
+    submitForm() {
+      this.loading = true
+      setTimeout(() => {
+        this.loading = false
+      }, 1200)
+    },
+    showGuide() {
+      console.log('show guide ...')
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.detail-container {
+  padding: 40px 50px 20px;
+  .preview-img {
+    width: 200px;
+    height: 270px;
+  }
+}
+</style>
